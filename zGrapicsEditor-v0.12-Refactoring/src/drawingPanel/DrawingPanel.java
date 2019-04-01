bea204cc2fdded09dcd45a40e6a093b49ea40a5c
sh199 <sh199@Solea>
2019-04-01 10:27:24 +0900

    0401

--- zGrapicsEditor-v0.12-Refactoring/src/drawingPanel/DrawingPanel.java
+++ zGrapicsEditor-v0.12-Refactoring/src/drawingPanel/DrawingPanel.java
@@ -22,6 +22,7 @@ public class DrawingPanel extends JPanel {
 	}
 
 	public DrawingPanel() {
+		this.eActionState = EActionState.eReady;
 		this.setBackground(Color.WHITE);
 		mouseHandler = new MouseHandler();
 		this.addMouseListener(this.mouseHandler);
@@ -29,27 +30,53 @@ public class DrawingPanel extends JPanel {
 		currentTool = EToolBar.rect.getShape();
 	}
 	
+	private enum EActionState{eReady, eCMCDrawing, ePDRDrawing};//레디, 폴리곤(nPointDrawing) , 나머지(2PointDrawing)
+	EActionState eActionState;
+	
 	private class MouseHandler implements MouseListener, MouseMotionListener {//함수 호출 만 하게 하라.
 		public void mousePressed(MouseEvent e) {
-			initDrawing(e.getX(), e.getY());
+			if(eActionState==EActionState.eReady) {
+				initDrawing(e.getX(), e.getY());
+				eActionState=EActionState.ePDRDrawing;
+			}
 		}
 		public void mouseDragged(MouseEvent e) {
-			keepDrawing(e.getX(), e.getY());
+			if(eActionState==EActionState.ePDRDrawing) {
+				keepDrawing(e.getX(), e.getY());
+			}
 		}
 		public void mouseReleased(MouseEvent e) {
-			finishDrawing(e.getX(), e.getY());
+			if(eActionState==EActionState.ePDRDrawing) {
+				initDrawing(e.getX(), e.getY());
+				eActionState=EActionState.eReady;
+			}
 		}
 		public void mouseClicked(MouseEvent e) {
 			if(e.getClickCount()==1) {
-				//init
-				//continue
+				mouse1Clicked(e);
 			}else if(e.getClickCount()==2) {
-				//finish
+				mouse2Clicked(e);
 			}
+		}
+		private void mouse1Clicked(MouseEvent e) {
+			if(eActionState==EActionState.eReady) {
+				initDrawing(e.getX(), e.getY());
+				eActionState=EActionState.eCMCDrawing;
+			}else if(eActionState==EActionState.eCMCDrawing){
+				finishDrawing(e.getX(), e.getY());
+				eActionState=EActionState.eReady;
+			}
+		}
+		private void mouse2Clicked(MouseEvent e) {
+			
 		}
 		public void mouseEntered(MouseEvent e) {}
 		public void mouseExited(MouseEvent e) {}
-		public void mouseMoved(MouseEvent e) {}
+		public void mouseMoved(MouseEvent e) {
+			if(eActionState==EActionState.eCMCDrawing) {
+				keepDrawing(e.getX(), e.getY());
+			}
+		}
 	}
 	
 	public void drawShape() {
@@ -74,8 +101,8 @@ public class DrawingPanel extends JPanel {
 	}
 	
 	private void finishDrawing(int x, int y) {
-		drawShape();
-		currentTool.setPoint(x, y);
-		drawShape();
+//		drawShape();
+//		currentTool.setPoint(x, y);
+//		drawShape();
 	}
 }
